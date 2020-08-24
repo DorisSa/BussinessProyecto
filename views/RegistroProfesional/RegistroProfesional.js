343ed84217f5a791fd453b38ea849e251105b556
DorisSa <elisantacruz26@gmail.com>
2020-08-23 21:04:18 -0500

    cambios generales

--- views/RegistroProfesional/RegistroProfesional.js
+++ views/RegistroProfesional/RegistroProfesional.js
@@ -29,6 +29,7 @@ function enviarRp(event){
 		document.getElementById('emailRp').value="";
 		document.getElementById('password2').value="";
 		document.getElementById('terminosRp').checked=0;
+		window.location.href ="../RedProfesional/InicioProfesional.html";
 
 
 	}
